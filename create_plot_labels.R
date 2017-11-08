1e3b99e2cc97df588315386f4e0766fec4d91b19
T <thomassocska@yahoo.com>
2017-11-09 00:03:48 +0100

    Remove breaking bugs

--- create_plot_labels.R
+++ create_plot_labels.R
@@ -9,11 +9,16 @@ library(stringr)
 
 create_plot_labels <- function(df) {
     df %>%
-        filter(!str_detect(value, "very unlikely")) %>% # Exclude very unlikely predictions
+        # filter(!str_detect(value, "very unlikely")) %>% # Exclude very unlikely predictions
         group_by(id, xmax, xmin, ymax, ymin) %>%
         summarise(label = paste0(emotion, ": ", value) %>% paste(collapse = "\n")) %>% 
         full_join(df %>% select(-emotion, -value), by = c("id","xmax","xmin","ymax","ymin")) %>% 
-        filter(!duplicated(id)) %>% 
-        mutate(label = if_else(is.na(label), "not recognized", label))
+        filter(!duplicated(id)) # %>% 
+        # mutate(label = if_else(is.na(label), "not recognized", label))
 }
 
+# Get sample pictures for each emotion
+selected_pictures %>% 
+    group_by(emotion) %>% 
+    slice(1) %>% 
+    select(image_file)
